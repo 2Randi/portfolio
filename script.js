13e397855151a13c4e60e2758882d46fe49ff26e
Fano R. <148653149+2Randi@users.noreply.github.com>
2025-08-29 20:26:41 +0200

    test avant final

--- script.js
+++ script.js
@@ -294,6 +294,8 @@ const canvas = document.getElementById('cv-canvas');
 const ctx = canvas.getContext('2d');
 const pdfUrl = 'CV.pdf';
 
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.12.313/pdf.worker.min.js';
+
 openCv.onclick = function(e) {
   e.preventDefault();
   cvModal.style.display = "flex";
